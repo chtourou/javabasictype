7bf0604effd01c014a59ad1bf2b8fb5fd7f17165
chtourou fathi <fathifatouh.youmait@gmail.com>
2017-04-05 16:09:04 +0100

    this is a signe program

---
+++ src/Signe.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Signe {
+    public static void main(String[] args) {
+        System.out.println("signe");
+        Scanner scanner = new Scanner(System.in);
+        int x;
+        System.out.println("donner x: ");
+
+        x = scanner.nextInt();
+        {
+            if (x <= 0) {
+                System.out.println("negativ number");
+            } else {
+                System.out.println("positiv number");
+
+            }
+
+
+        }
+
+    }
+}

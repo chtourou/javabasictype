49feb6213de83ef703e920765211ef09fa9b0c26
chtourou fathi <fathifatouh.youmait@gmail.com>
2017-04-05 16:40:10 +0100

    fixin wording

--- src/Signe.java
+++ src/Signe.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 
 public class Signe {
     public static void main(String[] args) {
-        System.out.println("signe");
         Scanner scanner = new Scanner(System.in);
         while (true) {
             int x;
-            System.out.println("donner x: ");
+            System.out.println("Give x: ");
 
             x = scanner.nextInt();
             {
                 if (x <= 0) {
-                    System.out.println("negativ number");
+                    System.out.println("negative number");
                 } else {
-                    System.out.println("positiv number");
+                    System.out.println("positive number");
 
                 }
 

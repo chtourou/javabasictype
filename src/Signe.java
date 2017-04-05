e7770090381d618b2a207a9e9804273ab9df1a65
chtourou fathi <fathifatouh.youmait@gmail.com>
2017-04-05 16:33:10 +0100

    boucle while

--- src/Signe.java
+++ src/Signe.java
@@ -4,20 +4,22 @@ public class Signe {
     public static void main(String[] args) {
         System.out.println("signe");
         Scanner scanner = new Scanner(System.in);
-        int x;
-        System.out.println("donner x: ");
+        while (true) {
+            int x;
+            System.out.println("donner x: ");
 
-        x = scanner.nextInt();
-        {
-            if (x <= 0) {
-                System.out.println("negativ number");
-            } else {
-                System.out.println("positiv number");
+            x = scanner.nextInt();
+            {
+                if (x <= 0) {
+                    System.out.println("negativ number");
+                } else {
+                    System.out.println("positiv number");
+
+                }
+
 
             }
 
-
         }
-
     }
 }

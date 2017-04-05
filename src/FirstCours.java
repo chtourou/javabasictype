fffe2aac3a040899ea85fd8c9f79c0197c360d42
chtourou fathi <fathifatouh.youmait@gmail.com>
2017-04-05 11:37:54 +0100

    Sum function

--- src/FirstCours.java
+++ src/FirstCours.java
@@ -1,12 +1,21 @@
+import java.util.Scanner;
 
 public class FirstCours {
     public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
         int x;
         int y;
         int z;
-        x = 5;
-        y = 3;
+
+        System.out.println("Donner X: ");
+        x = scanner.nextInt();
+        System.out.println("Donner Y: ");
+        y = scanner.nextInt();
+
         z = x + y;
-        System.out.println("The result is: " + z);
+
+        System.out.println("The the result is: " + z);
     }
 }

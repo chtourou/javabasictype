335114cb5d5cc9450c3b93e1f7ff2a2cb911a74d
chtourou fathi <fathifatouh.youmait@gmail.com>
2017-04-07 17:07:52 +0100

    map

--- src/CangedBase16.java
+++ src/CangedBase16.java
@@ -1,41 +1,50 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CangedBase16 {
     public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(0, "0");
+        map.put(1, "1");
+        map.put(2, "2");
+        map.put(3, "3");
+        map.put(4, "4");
+        map.put(5, "5");
+        map.put(6, "6");
+        map.put(7, "7");
+        map.put(8, "8");
+        map.put(9, "9");
+        map.put(10, "A");
+        map.put(11, "B");
+        map.put(12, "C");
+        map.put(13, "D");
+        map.put(14, "E");
+        map.put(15, "F");
+
+        int base = 13;
+
         Scanner input = new Scanner(System.in);
-        System.out.println("base 10 to 16");
-        int h;
+        System.out.println("base 10 to " + base);
+        int value;
         while (true) {
             System.out.println("give me a dicimal number positve h=");
-            h = input.nextInt();
-            boolean ishvalid = h >= 0;
-            if (ishvalid) {
-                String number = "";
-                while (h >0) {
-                    int rest = h % 16;
-                    h = h / 16;
-                    if (rest >= 0 && rest <= 9) {
+            value = input.nextInt();
+            boolean isValueValid = value >= 0;
 
-                        number = number + rest;
-                    } else if (rest == 10) {
-                        number = number + "A";
-                    } else if (rest == 11) {
-                        number = number + "B";
-                    } else if (rest == 12) {
-                        number = number + "C";
-                    } else if (rest == 13) {
-                        number = number + "D";
-                    } else if (rest == 14) {
-                        number = number + "E";
-                    } else if (rest == 15) {
-                        number = number + "F";
-                    }
-
-
-                }
-                System.out.println("Result: " + number);
-
+            if (!isValueValid) {
+                System.out.println("You entered a non valid number!");
+                continue;
             }
+
+            String number = "";
+            while (value > 0) {
+                int rest = value % base;
+                value = value / base;
+                number = map.get(rest) + number;
+            }
+            System.out.println("Result: " + number);
 
         }
 
---
+++ src/ResolutionEquationOfSecandDegre.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ResolutionEquationOfSecandDegre {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}

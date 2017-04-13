578776f7cab2b5bf91e3eedf8e4195633d63a6c6
chtourou <fathifatouh.youmait@gmail.com>
2017-04-13 16:03:27 +0100

    Functions

---
+++ src/LesFonction.java
@@ -0,0 +1,13 @@
+public class LesFonction {
+    public static void main(String[] args){
+        System.out.println(f(8));
+    }
+
+
+
+    public static  int f(int x){
+        return x+1;
+    }
+}
+
+

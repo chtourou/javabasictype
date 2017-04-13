3ecf39e457b8d43916f789877deaf4534b7fad3b
chtourou <fathifatouh.youmait@gmail.com>
2017-04-13 18:16:15 +0100

    boolean

---
+++ src/Boolean.java
@@ -0,0 +1,8 @@
+
+public class Boolean {
+    public static void main(String[] args) {
+        int i = 5;
+        boolean b = i == 7;
+        System.out.println(b);
+    }
+}
--- src/LesFonction.java
+++ src/LesFonction.java
@@ -1,12 +1,11 @@
 public class LesFonction {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println(f(8));
     }
 
 
-
-    public static  int f(int x){
-        return x+1;
+    public static int f(int x) {
+        return x + 1;
     }
 }
 
